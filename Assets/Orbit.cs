e18f62477dda3970985adea0ae7819d1ef80a412
SnowZucc <louis.alexandre@eleve.isep.fr>
2024-01-27 22:47:18 +0200

    Slower orbit

--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -8,7 +8,7 @@ public class Breakout : MonoBehaviour
     void Update()
     {
         // Orbit around the main sphere
-        transform.RotateAround(Vector3.zero, Vector3.up, 50 * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
         // Change color of the sphere every frame
         GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1);
     }
