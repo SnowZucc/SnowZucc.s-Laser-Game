f635593c96371a58f1adceabcdf1c7a61de12b7f
SnowZucc <louis.alexandre@eleve.isep.fr>
2024-02-29 16:12:07 +0200

    Cleanup for hw3, imported map, enemy, gun and lever, created walking animation, walking script and pathfinding.

---
+++ Assets/My assets/Scripts/NavigationScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationScript : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent=GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+        animator.SetFloat("speed", agent.velocity.magnitude);
+    }
+}
