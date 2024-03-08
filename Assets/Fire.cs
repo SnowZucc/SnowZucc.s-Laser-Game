3bc04fd2dcfd39d2afba4b79f008d158b1865c53
SnowZucc <louis.alexandre@eleve.isep.fr>
2024-03-08 16:38:19 +0200

    Working grab an interraction sytem, working laser gun with sound and music

---
+++ Assets/Fire.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 50;
+    public AudioClip[] fireSounds; // The sound effects
+    private AudioSource audioSource; // The audio source
+    private bool canFire = true; // Whether the gun can be fired
+    private float fireCooldown = 0.5f; // The cooldown time in seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+        audioSource = GetComponent<AudioSource>(); // Get the audio source
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        if (canFire)
+        {
+            GameObject spawnedBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+            spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+            Destroy(spawnedBullet, 0.4f);
+
+            // Select a random sound effect
+            AudioClip fireSound = fireSounds[Random.Range(0, fireSounds.Length)];
+            audioSource.PlayOneShot(fireSound); // Play the sound effect
+
+            // Start the cooldown
+            StartCoroutine(FireCooldown());
+        }
+    }
+
+    private IEnumerator FireCooldown()
+    {
+        canFire = false; // Prevent firing
+        yield return new WaitForSeconds(fireCooldown); // Wait for the cooldown time
+        canFire = true; // Allow firing again
+    }
+}
